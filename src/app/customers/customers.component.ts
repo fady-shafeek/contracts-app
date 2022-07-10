b1a212c502b2bdd90b87a9b1be17a6420f0e45fd
fady-shafeek <Fady.Shafeek@hotmail.com>
2022-07-10 22:00:57 +0200

    Delete unusable code

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -10,22 +10,15 @@ export class CustomersComponent implements OnInit {
   constructor(private _customers: CustomersService) {}
 
   result: any;
-  page = 1;
-  pageSize = 5;
-  collectionSize :number =1000;
 
   ngOnInit(): void {
     this._customers.getCustomers().subscribe((res) => {
       this.result = res;
-      this.collectionSize = res.length;
     }); 
-    this.refreshCountries();
+  
+  
   }
 
-
-  refreshCountries() {
-    // this.countries = COUNTRIES.map((country, i) => ({id: i + 1, ...country})).slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
-  }
 
 
 
